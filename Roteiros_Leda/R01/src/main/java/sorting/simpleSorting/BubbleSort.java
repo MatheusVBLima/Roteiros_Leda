21971289b99b36393b8f07794b3dc0b00794681a
Matheus Lima <55745915+MatheusVBLima@users.noreply.github.com>
2021-07-22 21:17:54 -0300

    Update BubbleSort.java

--- Roteiros_Leda/R01/src/main/java/sorting/simpleSorting/BubbleSort.java
+++ Roteiros_Leda/R01/src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -11,7 +11,30 @@ public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if (this.validation(array, leftIndex, rightIndex)) {
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				for (int j = leftIndex; j < rightIndex; j++) {
+					if (array[j].compareTo(array[j + 1]) > 0) {
+						swap(array, j, j + 1);
+					}
+				}
+			}
+		}
+	}
+
+	private boolean validation(T[] array, int leftIndex, int rightIndex) {
+		boolean isValid = true;
+
+		if (array == null || array.length == 0) {
+			isValid = false;
+		} else if ((leftIndex >= rightIndex) || (leftIndex < 0) || (rightIndex <= 0)) {
+			isValid = false;
+		} else if ((rightIndex > array.length - 1) || leftIndex >= array.length) {
+			isValid = false;
+		}
+
+		return isValid;
+	}
+
 	}
 }
